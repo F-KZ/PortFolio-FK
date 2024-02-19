0fb87d70e343938ab7efd2d2ac8c2084283e0492
F-KZ <supreme-soul@hotmail.fr>
2024-02-19 15:53:13 +0100

    cv maj

--- src/components/draw/DrawSection.jsx
+++ src/components/draw/DrawSection.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import cvPic from "../../img/cvPic.png"
 
 
-const PDF_CV = "https://port-folio-fkkp2cupy-fkz.vercel.app/CV.pdf"
+const PDF_CV = "https://port-folio-fkkp2cupy-fkz.vercel.app/Franck-CV.pdf"
 
 export const DrawSection = () => {
 
